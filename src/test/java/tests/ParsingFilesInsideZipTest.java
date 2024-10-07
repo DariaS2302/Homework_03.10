72b081288b5786fd2236ea39f8878030f7f103c9
Sarycheva Daria <d.saricheva@gmail.com>
2024-10-07 16:37:57 +0300

    add EditFileTest_v2

--- src/test/java/tests/ParsingFilesInsideZipTest.java
+++ src/test/java/tests/ParsingFilesInsideZipTest.java
@@ -3,7 +3,6 @@ package tests;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
